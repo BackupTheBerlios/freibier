35a45114ef3d3806e2a3128e4b60d13a0736eb34
susannewenz <susannewenz>
2005-08-26 20:59:11 +0000

    drucken eingefügt

--- FiBuGUI/src/de/bayen/fibu/gui/menu/OffenesJournalMenu.java
+++ FiBuGUI/src/de/bayen/fibu/gui/menu/OffenesJournalMenu.java
@@ -1,9 +1,10 @@
-// $Id: OffenesJournalMenu.java,v 1.2 2005/08/23 19:40:15 phormanns Exp $
+// $Id: OffenesJournalMenu.java,v 1.3 2005/08/26 20:59:11 susannewenz Exp $
 package de.bayen.fibu.gui.menu;
 
 import de.bayen.fibu.gui.action.BuchenAction;
 import de.bayen.fibu.gui.action.BuchungenAction;
 import de.bayen.fibu.gui.action.JournalAbsummierenAction;
+import de.bayen.fibu.gui.action.JournalDruckenAction;
 import de.bayen.fibu.gui.action.JournalLoeschenAction;
 import de.bayen.fibu.gui.widget.ContextMenu;
 import de.willuhn.jameica.gui.parts.ContextMenuItem;
@@ -13,6 +14,7 @@ public class OffenesJournalMenu extends ContextMenu {
 	public OffenesJournalMenu() {
 		addItem(new ContextMenuItem("buchen", new BuchenAction()));
 		addItem(new ContextMenuItem("zeigen", new BuchungenAction()));
+		addItem(new ContextMenuItem("drucken", new JournalDruckenAction()));
 		addItem(ContextMenuItem.SEPARATOR);
 		addItem(new ContextMenuItem("löschen", new JournalLoeschenAction()));
 		addItem(new ContextMenuItem("absummieren", new JournalAbsummierenAction()));
@@ -21,6 +23,9 @@ public class OffenesJournalMenu extends ContextMenu {
 
 /*
  *  $Log: OffenesJournalMenu.java,v $
+ *  Revision 1.3  2005/08/26 20:59:11  susannewenz
+ *  drucken eingefügt
+ *
  *  Revision 1.2  2005/08/23 19:40:15  phormanns
  *  Abhängigkeiten vom Willuhn-Persistenzframework  durch Kopieren und Anpassen einiger Widgets entfernt
  *
