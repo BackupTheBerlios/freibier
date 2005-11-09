bbecb7aab52d31609085af701713c79e262aba39
phormanns <phormanns>
2005-11-09 23:01:35 +0000

    Menubar hinzugefügt

--- FiBuSwing/src/de/jalin/fibu/gui/FiBuGUI.java
+++ FiBuSwing/src/de/jalin/fibu/gui/FiBuGUI.java
@@ -1,15 +1,19 @@
-// $Id: FiBuGUI.java,v 1.1 2005/11/09 22:28:33 phormanns Exp $
+// $Id: FiBuGUI.java,v 1.2 2005/11/09 23:01:35 phormanns Exp $
 
 package de.jalin.fibu.gui;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Vector;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -21,8 +25,6 @@ import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
-
-
 import com.jgoodies.looks.LookUtils;
 import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
 import de.jalin.fibu.gui.tree.DynamicFolder;
@@ -34,18 +36,19 @@ public class FiBuGUI {
 
 	private JFrame frame;
 	private JPanel workArea;
+	private JTree treeMenu;
 
 	public FiBuGUI() {
 		frame = new JFrame("Freibier - Buchhaltung");
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
-				// TODO Auto-generated method stub
-				System.exit(0);
+				exitFiBu();
 			}
 		});
-		JTree menuTree = new JTree(initMenuTree());
-		menuTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-		menuTree.addTreeSelectionListener(new TreeSelectionListener() {
+		frame.setJMenuBar(initMenuBar());
+		treeMenu = new JTree(initTreeMenu());
+		treeMenu.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		treeMenu.addTreeSelectionListener(new TreeSelectionListener() {
 			public void valueChanged(TreeSelectionEvent e) {
 				// TODO Auto-generated method stub
 				boolean canLeaveOldNode = false;
@@ -66,11 +69,11 @@ public class FiBuGUI {
 					workArea.revalidate();
 					workArea.repaint();
 				} else {
-					((JTree) e.getSource()).setSelectionPath(oldPath);
+					treeMenu.setSelectionPath(oldPath);
 				}
 			}
 		});
-		JScrollPane treePane = new JScrollPane(menuTree);
+		JScrollPane treePane = new JScrollPane(treeMenu);
 		treePane.setPreferredSize(new Dimension(200, 600));
 		workArea = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JScrollPane formPane = new JScrollPane(workArea);
@@ -86,7 +89,24 @@ public class FiBuGUI {
 		frame.setVisible(true);
 	}
 
-	private TreeNode initMenuTree() {
+	private JMenuBar initMenuBar() {
+		JMenuBar mainMenu = new JMenuBar();
+		JMenu fileMenu = new JMenu("Datei");
+		JMenuItem prefsItem = new JMenuItem("Einstellungen");
+		fileMenu.add(prefsItem);
+		fileMenu.addSeparator();
+		JMenuItem exitItem = new JMenuItem("beenden");
+		exitItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent exitEvent) {
+				exitFiBu();
+			}
+		});
+		fileMenu.add(exitItem);
+		mainMenu.add(fileMenu);
+		return mainMenu;
+	}
+
+	private TreeNode initTreeMenu() {
 		StaticFolder root = new StaticFolder("FiBu");
 		StaticFolder journals = new StaticFolder("Journale");
 		journals.addFolder(new DynamicFolder("Offene Journale") {
@@ -100,6 +120,20 @@ public class FiBuGUI {
 		root.addFolder(new StaticFolder("Konten"));
 		root.addFolder(new StaticFolder("Auswertungen"));
 		return root;
+	}
+
+	private void exitFiBu() {
+		TreePath selectionPath = treeMenu.getSelectionPath();
+		if (selectionPath != null) {
+			Object leafNode = selectionPath.getLastPathComponent();
+			if (leafNode != null) {
+				if (((Editable) leafNode).validateAndSave()) {
+					System.exit(0);
+				}
+			}
+		} else {
+			System.exit(0);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -118,6 +152,9 @@ public class FiBuGUI {
 
 //
 // $Log: FiBuGUI.java,v $
+// Revision 1.2  2005/11/09 23:01:35  phormanns
+// Menubar hinzugefügt
+//
 // Revision 1.1  2005/11/09 22:28:33  phormanns
 // erster Import
 //
