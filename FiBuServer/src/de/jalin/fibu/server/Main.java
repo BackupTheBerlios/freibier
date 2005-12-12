b76dc5961fd93663d1a68fe46bc0163124aa8ae4
phormanns <phormanns>
2005-12-12 21:09:05 +0000

    Datenbank-Initialisierung begonnen

--- FiBuServer/src/de/jalin/fibu/server/Main.java
+++ FiBuServer/src/de/jalin/fibu/server/Main.java
@@ -1,11 +1,23 @@
-// $Id: Main.java,v 1.1 2005/11/20 21:27:44 phormanns Exp $
+// $Id: Main.java,v 1.2 2005/12/12 21:09:05 phormanns Exp $
 package de.jalin.fibu.server;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		try {
-			Server.startServer();
+			if (args.length == 1) {
+				String opt = args[0];
+				if ("server".equals(opt)) {
+					Server srv = new Server();
+					srv.startServer();
+					System.exit(0);
+				}
+				if ("initdb.model".equals(opt)) {
+					InitDB init = new InitDB();
+					init.initModel();
+					System.exit(0);
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -14,6 +26,9 @@ public class Main {
 
 /*
  *  $Log: Main.java,v $
+ *  Revision 1.2  2005/12/12 21:09:05  phormanns
+ *  Datenbank-Initialisierung begonnen
+ *
  *  Revision 1.1  2005/11/20 21:27:44  phormanns
  *  Import
  *
